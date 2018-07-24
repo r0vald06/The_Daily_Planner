c05fd2cf1cff32b6d2275e5bcfc642581f3580eb
Raudel Valdes <raudel.valdes2015@gmail.com>
2018-07-24 03:54:31 -0400

    added more css to the flickr images and set background

--- js/flickr.js
+++ js/flickr.js
@@ -1,27 +1,28 @@
-$(document).ready(function() {
-  
+$(document).ready(function() { // waits for the javascript to load first
   var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
  
- 
-  $('form').submit(function (evt) {
-     var $submitButton = $('#submit');
+  $('form').submit(function (event) {
+     var $submitButton = $('#submit'); //when the user hits enter rather hitting search button
      var $searchField = $('#search');
-     evt.preventDefault();
-     $searchField.prop("disabled",true);
-     $submitButton.attr("disabled", true).val("searching....");
-     var animal = $searchField.val();
+     
+     event.preventDefault(); //stops the page from refreshing itself
+     
+     $searchField.prop("disabled",true); //checking to see if the user as searched anything
+     $submitButton.attr("disabled", true).val("searching...."); //updating serch button text
+    
+     var search = $searchField.val();
      $('#photos').html('');
      $.getJSON(flickerAPI, {
-         tags: animal,
+         tags: search,
          format: "json"
        },
      function(data){
        var photoHTML = '';
        if (data.items.length > 0) {
          $.each(data.items,function(i,photo) {
-           photoHTML += '<li class="grid-25 tablet-grid-50">';
+          //  photoHTML += '<div>';
            photoHTML += '<a href="' + photo.link + '" class="image">';
-           photoHTML += '<img src="' + photo.media.m + '"></a></li>';
+           photoHTML += '<img class="imgCube"src="' + photo.media.m + '"></a></div>';
          }); // end each
        } else {
          photoHTML = "<p>No photos found that match: " + animal + ".</p>"
